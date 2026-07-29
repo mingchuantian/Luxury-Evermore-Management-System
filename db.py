50cc2d2e9996476ac09aa86d09a62a3ed57a8e40
Mingchuan Tian <1120509786@qq.com>
2026-07-30 01:15:43 +0800

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 def get_db():
-    mongo_uri = "mongodb://localhost:27017/"
-    db_name = "inventory_test"
+    mongo_uri = os.getenv("MONGO_URI")
+    db_name = os.getenv("MONGO_DB")
     client = MongoClient(mongo_uri)
     return client[db_name]
